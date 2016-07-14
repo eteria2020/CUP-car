ca731fefc766c70713bcf7c5ef58e5b37abc393f
Massimo De Luisa <massimo.deluisa@bulweria.com>
2016-07-14 19:35:21 +0200

    msg-router-service: Implemented MsgRouter Service Project.

--- MsgRouter/config.local.js
+++ MsgRouter/config.local.js
@@ -1,5 +1,13 @@
 // DataBase
-exports.pgDb = "postgres://sharengo:gmjk51pa@dm-1:5432/sharengo";
+exports.pgHost = "dm-1";
+exports.pgPort = 5432;
+exports.pgDbName = "sharengo";
+exports.pgUser = "sharengo";
+exports.pgPassw = "gmjk51pa";
+exports.pgConnectionTimeout = 5; // 5 seconds
+exports.pgDb = "postgres://" + exports.pgUser +
+    ":" + exports.pgPassw + "@" + exports.pgHost +
+    ":" + exports.pgPort + "/" + exports.pgDbName;
 exports.pgPoolSize = 25;
 exports.pgPoolIdleTimeout = 5000; // 5 sec
 
@@ -22,6 +30,7 @@ exports.globalConfigFilePath = "../config/globalConfig";
 
 // Telegram
 exports.telegramTokeId = "224402891:AAGop9luiZtrh6yW2X50ZpyboyrJ9mqBE4M";
+exports.telegramPrivateGroupID = -1001052334823;
 
 // DEBUG
 exports.debugMode = process.env.DEBUG === "true" || process.env.DEBUG === "1" || false;
