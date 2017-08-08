1daa9b741c048b07eb750516120a9adb324283c9
Fulvio <fulvio.bombara@sharengo.eu>
2017-08-08 16:20:35 +0200

    genarlized

--- config/globalConfig.js
+++
@@ -1,8 +0,0 @@
-// db
-exports.pgDb = "postgres://sharengo:Sharengo1@127.0.0.1/sharengo";
-exports.redisDb = "127.0.0.1:6379";
-exports.redisCluster =   [{host: '127.0.0.1', port: 26379}];
-exports.redisSentinels =  { sentinels: [{host: '127.0.0.1', port: 26379}],  name : 'localhost' };
-exports.logPath = "/var/log/services/";
-exports.conString = 'postgres://sharengo:Sharengo1@localhost/sharengo';
-
