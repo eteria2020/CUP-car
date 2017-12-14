73e9b9a5ee0c7d39f39afe27c3608692f6ad2b9c
momo <egyangel93@gmail.com>
2017-12-14 10:15:48 +0100

    Ultima modifica

--- CarWebServices/CarWebServices.js
+++ CarWebServices/CarWebServices.js
@@ -26,9 +26,9 @@
 
 
 // Basic modules
-var stamp =     require('console-stamp')(console, 'dd/mm/yyyy HH:MM:ss.l');
-var cluster =   require('cluster');
-var fs =        require('fs');
+var stamp = require('console-stamp')(console, 'dd/mm/yyyy HH:MM:ss.l');
+var cluster = require('cluster');
+var fs = require('fs');
 var validator = require('validator');
 
 
@@ -36,7 +36,7 @@ var validator = require('validator');
 // Configurations
 try {
     var config = require('./config');
-} catch(ex) {
+} catch (ex) {
     console.error("FATAL: Missing or invalid ./config.js ");
     exit(1);
 }
@@ -44,20 +44,20 @@ try {
 
 try {
     var globalConfig = require('../config/globalConfig');
-}   catch(ex) {
+} catch (ex) {
     console.warn("Missing or invalid ../config/globalConfig.js");
     var globalConfig = {};
 }
 
-var conString =         config.pgDb || globalConfig.pgDb;
-var redisServer =       config.redisServer || globalConfig.redisServer;
-var redisDb =           config.redisDb;
-var logPath =           config.logPath || globalConfig.logPath;
-var serverName =        config.serverName || 'CarWebServices';
-var standardPort =      config.httpPort || 8123;
-var unsecurePort =      config.httpUnsecurePort || 8121;
-var debugMode =         config.debugMode || false;
-var redisCluster =      globalConfig.redisCluster || [];
+var conString = config.pgDb || globalConfig.pgDb;
+var redisServer = config.redisServer || globalConfig.redisServer;
+var redisDb = config.redisDb;
+var logPath = config.logPath || globalConfig.logPath;
+var serverName = config.serverName || 'CarWebServices';
+var standardPort = config.httpPort || 8123;
+var unsecurePort = config.httpUnsecurePort || 8121;
+var debugMode = config.debugMode || false;
+var redisCluster = globalConfig.redisCluster || [];
 
 
 
@@ -67,26 +67,32 @@ var restify = require('restify');
 
 var pg = require('pg').native;
 pg.defaults.poolSize = 25;
-pg.defaults.poolIdleTimeout=5000; // 5 sec
+pg.defaults.poolIdleTimeout = 5000; // 5 sec
 
 var morgan = require('morgan');
 fs.existsSync(logPath) || fs.mkdirSync(logPath)
-var accessLogStream = fs.createWriteStream(logPath + 'CarWebServices_access.log', {flags: 'a'})
-var morganLogger = morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":user-agent" - :response-time ms ', { stream : accessLogStream})
+var accessLogStream = fs.createWriteStream(logPath + 'CarWebServices_access.log', { flags: 'a' })
+var morganLogger = morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":user-agent" - :response-time ms ', { stream: accessLogStream })
 
 
 var bunyan = require('bunyan');
 var log = bunyan.createLogger({
-  name: "ws",
-  streams: [
-    { path : logPath + 'CarWebServices.log' }
-  ],
-  serializers: restify.bunyan.serializers
+    name: "ws",
+    streams: [
+        { path: logPath + 'CarWebServices.log' }
+    ],
+    serializers: restify.bunyan.serializers
 });
 
 var dlog = {
-  d : function(msg){ console.log(msg); log.info(msg)},
-  e : function(){ console.error(arguments); log.error(arguments)},
+    d: function(msg) {
+        console.log(msg);
+        log.info(msg)
+    },
+    e: function() {
+        console.error(arguments);
+        log.error(arguments)
+    },
 }
 
 
@@ -95,7 +101,7 @@ var dlog = {
 var server;
 var unsecureServer;
 
-var funcs = require('./restFunctions')( {pg: pg , conString: conString, validator: validator});
+var funcs = require('./restFunctions')({ pg: pg, conString: conString, validator: validator });
 
 
 // Local functions
@@ -103,85 +109,85 @@ var funcs = require('./restFunctions')( {pg: pg , conString: conString, validato
 
 
 /**
-* Send log row to API log queue
-* @param  string   ip  client ip address
-* @param  string   route  called route
-* @param  string   plate  Car plate
-* @param  string   payload  Log body
-*/
+ * Send log row to API log queue
+ * @param  string   ip  client ip address
+ * @param  string   route  called route
+ * @param  string   plate  Car plate
+ * @param  string   payload  Log body
+ */
 function ApiLog(ip, route, plate, payload) {
-  var xpayload = { timestamp : new Date() , host : ip , pid : process.pid , payload : payload };
-  dlog.d(xpayload);
+    var xpayload = { timestamp: new Date(), host: ip, pid: process.pid, payload: payload };
+    dlog.d(xpayload);
 
-  //console.log("JSON2 lenght=" + xpayload.length + xpayload );
-  /*
-  jkue.create('log_api', {
-    title : "Api :" + xpayload.timestamp,
-    payload : xpayload
-  }).save(function (err) {
-    if (err)
-        console.error(err);
-    else
-        console.log("Queued : " + xpayload);
-  });
-  */
+    //console.log("JSON2 lenght=" + xpayload.length + xpayload );
+    /*
+    jkue.create('log_api', {
+      title : "Api :" + xpayload.timestamp,
+      payload : xpayload
+    }).save(function (err) {
+      if (err)
+          console.error(err);
+      else
+          console.log("Queued : " + xpayload);
+    });
+    */
 }
 
 
 
 /**
-* Send log row to API log queue
-* @param  object   server  restify instance to initialize
-*/
+ * Send log row to API log queue
+ * @param  object   server  restify instance to initialize
+ */
 
 function registerServer(server) {
 
-	//server.use(restify.acceptParser(server.acceptable));
-	server.use(restify.authorizationParser());
-	//server.use(restify.dateParser());
-	server.use(restify.queryParser());
-	//server.use(restify.jsonp());
-	server.use(restify.gzipResponse());
-	server.use(restify.bodyParser());
-	server.use(restify.throttle({
-	    burst: 32,
-	    rate: 200,
-	    ip: true,
-	    /*overrides: {
+    //server.use(restify.acceptParser(server.acceptable));
+    server.use(restify.authorizationParser());
+    //server.use(restify.dateParser());
+    server.use(restify.queryParser());
+    //server.use(restify.jsonp());
+    server.use(restify.gzipResponse());
+    server.use(restify.bodyParser());
+    server.use(restify.throttle({
+        burst: 32,
+        rate: 200,
+        ip: true,
+        /*overrides: {
 		    '192.168.1.1': {
 		      rate: 0,        // unlimited
 		      burst: 0
 		    }
 		  }*/
-	}));
-	//server.use(restify.conditionalRequest());
-	//server.use(restify.CORS());
+    }));
+    //server.use(restify.conditionalRequest());
+    //server.use(restify.CORS());
 
     server.use(morganLogger);
 
     server.pre(restify.pre.sanitizePath());
-    server.pre(function (request, response, next) {
+    server.pre(function(request, response, next) {
         next();
-	});
+    });
 
     // Handlers
 
-	server.on('InternalServerError', function(req, res, err, cb) {
-	    err._customContent = 'Error';
-        dlog.e('InternalServerError',err);
-	    return cb();
-	});
-	server.on('InternalError', function(req, res, err, cb) {
-	    err._customContent = 'Error';
-        dlog.e('InternalError',err);
-	    return cb();
-	});
+    server.on('InternalServerError', function(req, res, err, cb) {
+        err._customContent = 'Error';
+        dlog.e('InternalServerError', err);
+        return cb();
+    });
+    server.on('InternalError', function(req, res, err, cb) {
+        err._customContent = 'Error';
+        dlog.e('InternalError', err);
+        return cb();
+    });
 
-	server.on('ResourceNotFoundError', function(req, res, err, cb) {
-	    err._customContent = 'Not found';
-        dlog.e('ResourceNotFound',err);
-	    return cb();
-	});
+    server.on('ResourceNotFoundError', function(req, res, err, cb) {
+        err._customContent = 'Not found';
+        dlog.e('ResourceNotFound', err);
+        return cb();
+    });
     /*
     server.on('uncaughtException', function (req, res, route, err) {
       console.log('======= server uncaughtException');
@@ -190,91 +196,94 @@ function registerServer(server) {
     });
     */
 
-    server.on('after', function (request, response, route, error) {
-	    request.log.info({ req: request,params:request.params }, 'REQUEST');
+    server.on('after', function(request, response, route, error) {
+        request.log.info({ req: request, params: request.params }, 'REQUEST');
 
-        ApiLog(request.connection.remoteAddress, request.url,"USER" , request.params);      //request.authorization.basic.username
+        ApiLog(request.connection.remoteAddress, request.url, "USER", request.params); //request.authorization.basic.username
     });
 
 
     // Routes
 
-        /*
+    /*
 	server.get(
 		{path: '/admins', version: '1.0.0'},
 		funcs.getAdmins
 	);
 	*/
 
-	server.get(
-        {path: '/configs', version: '1.0.0'},
-		funcs.getConfigs
-	);
+    server.get({ path: '/configs', version: '1.0.0' },
+        funcs.getConfigs
+    );
 
-        server.get(
-        {path: '/whitelist', version: '1.0.0'},
-		funcs.getWhitelist
-	);
+    server.get({ path: '/whitelist', version: '1.0.0' },
+        funcs.getWhitelist
+    );
+    server.get({ path: '/whitelist2', version: '1.0.0' },
+        funcs.getWhitelist2
+    );
 
-
+    server.get({ path: '/business-employees', version: '1.0.0' },
+        funcs.getBusinessEmployees
+    );
 }
 
 
 // Main
 
 
-    dlog.d('Webservice startup');
+dlog.d('Webservice startup');
 
-    /*
-    process.on('uncaughtException', function (err) {
-      console.log('==== process uncaughtException');
-      err = err || {};
-      console.log('======== ', arguments);
+/*
+process.on('uncaughtException', function (err) {
+  console.log('==== process uncaughtException');
+  err = err || {};
+  console.log('======== ', arguments);
 
-    });
-    */
-
-
-    var responseFormatter = {
-            'application/json': function customizedFormatJSON( request, res, body ,cb ) {
+});
+*/
 
 
-	            if ( body instanceof Error ) {
-	                res.statusCode = body.statusCode || 500;
-
-	                if ( body.body ) {
-	                	console.log('\nERROR\n\n===============\n');
-	                	console.log(body);
-	                	res.statusCode = 400;
-	                    body = {
-	                        status: 400,
-	                        reason: "Invalid parameters",
-	                        time: Date.now() / 1000 | 0
-	                    };
-	                } else {
-	                    body = {
-	                        msg: body.message
-	                    };
-	                }
-	            } else if ( Buffer.isBuffer( body ) ) {
-	                body = body.toString( 'base64' );
-	            }
-
-	            var data = JSON.stringify( body );
-                res.setHeader( 'Content-Length', Buffer.byteLength( data ) );
-
-                var auth = funcs.parseAuth(request)
-                console.log(auth);
-
-                //var username = request.authorization?request.authorization.basic.username:"UNKNOWN";
-                var username = "USERNAME";
-                dlog.d(request.method + " " + request.url + " - " + auth.username + " " + res.statusCode  +" " + res.getHeader('Content-Length'));
-                dlog.d("Body: " + (data.length <= 16 ? data : data.substr(0,15)+'...')+ "  [" + Buffer.byteLength( data ) + "]");
+var responseFormatter = {
+    'application/json': function customizedFormatJSON(request, res, body, cb) {
 
 
-	            return cb(null,data);
+        if (body instanceof Error) {
+            res.statusCode = body.statusCode || 500;
+
+            if (body.body) {
+                console.log('\nERROR\n\n===============\n');
+                console.log(body);
+                res.statusCode = 400;
+                body = {
+                    status: 400,
+                    reason: "Invalid parameters",
+                    time: Date.now() / 1000 | 0
+                };
+            } else {
+                body = {
+                    msg: body.message
+                };
             }
-    };
+        } else if (Buffer.isBuffer(body)) {
+            body = body.toString('base64');
+        }
+
+        var data = JSON.stringify(body);
+        res.setHeader('Content-Length', Buffer.byteLength(data));
+
+        var auth = funcs.parseAuth(request)
+        console.log(auth);
+
+        //var username = request.authorization?request.authorization.basic.username:"UNKNOWN";
+        var username = "USERNAME";
+        dlog.d(request.method + " " + request.url + " - " + auth.username + " " + res.statusCode + " " + res.getHeader('Content-Length'));
+        dlog.d("Body: " + (data.length <= 16 ? data : data.substr(0, 15) + '...') + "  [" + Buffer.byteLength(data) + "]");
+
+
+        return cb(null, data);
+    }
+};
 
 /*
     if (cluster.isMaster) {
@@ -286,39 +295,35 @@ function registerServer(server) {
        })
     } else {
 */
-	server = restify.createServer({
-        name: serverName,
-	    formatters: responseFormatter,
-	    log: log,
-	    certificate: fs.readFileSync('../ssl/server.cer'),
-	    key: fs.readFileSync('../ssl/server.key'),
-        ca:  fs.readFileSync('../ssl/ca.cer'),
-        requestCert:        true,
-        rejectUnauthorized: true
-	});
-    dlog.d('Created standard server');
+server = restify.createServer({
+    name: serverName,
+    formatters: responseFormatter,
+    log: log,
+    certificate: fs.readFileSync('../ssl/server.cer'),
+    key: fs.readFileSync('../ssl/server.key'),
+    ca: fs.readFileSync('../ssl/ca.cer'),
+    requestCert: true,
+    rejectUnauthorized: true
+});
+dlog.d('Created standard server');
 
 
-    unsecureServer = restify.createServer({
-	    name: serverName,
-	    formatters: responseFormatter,
-	    log: log
-	});
-    dlog.d('Created unsecure debug server');
+unsecureServer = restify.createServer({
+    name: serverName,
+    formatters: responseFormatter,
+    log: log
+});
+dlog.d('Created unsecure debug server');
 
-    registerServer(server);
-    registerServer(unsecureServer);
+registerServer(server);
+registerServer(unsecureServer);
 
-    server.listen({host:'0.0.0.0',port:standardPort});
-    dlog.d('Listen standard server: ' + standardPort)
+server.listen({ host: '0.0.0.0', port: standardPort });
+dlog.d('Listen standard server: ' + standardPort)
 
 
-    unsecureServer.listen({host:'0.0.0.0',port:unsecurePort});
-    dlog.d('Listen unsecure debug server: ' + unsecurePort);
+unsecureServer.listen({ host: '0.0.0.0', port: unsecurePort });
+dlog.d('Listen unsecure debug server: ' + unsecurePort);
 
-    dlog.d("Worker started...");
+dlog.d("Worker started...");
 //}
-
-
-
-
--- CarWebServices/restFunctions.js
+++ CarWebServices/restFunctions.js
@@ -121,7 +121,6 @@ function init(opt) {
         },
 
 
-
         /**
          * get pois
          * @param  array   req  request
@@ -137,27 +136,26 @@ function init(opt) {
                     return next(false);
                 }
 
-                var query = '',
-                    params = [],
-                    queryString = '',
-                    isSingle = false;
-
-                query =
+                var query =
                     "SELECT " +
-                    "id as i," +
-                    "name as n," +
-                    "surname as c," +
-                    "mobile as t," +
-                    "enabled as a," +
-                    "CASE WHEN maintainer=true THEN 'sharengo' ELSE ''  END as id," +
-                    "pin->'primary' as pin," +
-                    "card_code as cd," +
-                    "update_id  as tm" +
+                    "id," +
+                    "name as nome," +
+                    "surname as cognome," +
+                    "language as lingua," +
+                    "mobile as cellulare ," +
+                    "enabled as abilitato," +
+                    "CASE WHEN maintainer=true THEN 'sharengo' ELSE ''  END as info_display," +
+                    "substring(md5(cast(pin->'primary' as text)) for 8) as pin," +
+                    "'' as pin2," +
+                    "pin as pins," +
+                    "card_code as codice_card," +
+                    "update_id  as tms " +
                     " FROM customers  " +
                     " WHERE update_id > $1 " +
                     " ORDER BY update_id " +
                     " LIMIT 10000";
 
+                var params = [];
                 if (typeof req.params.lastupdate === 'undefined')
                     params = [0];
                 else
@@ -171,19 +169,17 @@ function init(opt) {
                             responseError(res, err);
                         } else {
                             done();
-                            var outTxt = '',
-                                outJson = '[]';
-                            var configs = {};
-                            if ((typeof result !== 'undefined')) {
+                            if (typeof result !== 'undefined') {
                                 result.rows.forEach(function(item) {
-                                    // For each row compute hash for each pin in JSON structure
-                                    Object.keys(item.pins).forEach(function(key) {
-                                        var pin = "" + item.pins[key];
-                                        item.pins[key] = crypto.createHash('md5').update(pin).digest("hex").substring(0, 8);
+                                    // For each row compute hash for each possible pin
+                                    var a = ["primary", "secondary", "company"];
+                                    a.forEach(function(key) {
+                                        if (item.pins[key]) {
+                                            var pin = "" + item.pins[key];
+                                            item.pins[key] = crypto.createHash('md5').update(pin).digest("hex").substring(0, 8);
+                                        }
                                     });
                                 });
-
-                                outJson = JSON.stringify(result.rows);
                             }
                             res.header('content-type', 'application/json');
                             res.send(200, result.rows);
@@ -192,6 +188,108 @@ function init(opt) {
                 );
             });
             //}
+            return next();
+        },
+
+        /**
+         * get pois
+         * @param  array   req  request
+         * @param  array   res  response
+         * @param  function next handler
+         */
+        getWhitelist2: function(req, res, next) {
+            //if(sanitizeInput(req,res)){
+            pg.connect(conString, function(err, client, done) {
+
+                if (pgError(err, client)) {
+                    responseError(res, err);
+                    return next(false);
+                }
+
+                var query =
+                    "SELECT " +
+                    "id as i," +
+                    "name as n," +
+                    "surname as c," +
+                    "mobile as t ," +
+                    "enabled as a," +
+                    "CASE WHEN maintainer=true THEN 'sharengo' ELSE ''  END as id," + // infoDisplay
+                    "substring(md5(cast(pin->'primary' as text)) for 8) as p," +
+                    "pin as ps," +
+                    "card_code as cc," +
+                    "update_id  as tm " +
+                    " FROM customers  " +
+                    " WHERE update_id > $1 " +
+                    " ORDER BY update_id " +
+                    " LIMIT 10000";
+
+                var params = [];
+                if (typeof req.params.lastupdate === 'undefined')
+                    params = [0];
+                else
+                    params = [req.params.lastupdate];
+
+                client.query(
+                    query,
+                    params,
+                    function(err, result) {
+                        if (pgError(err, client)) {
+                            responseError(res, err);
+                        } else {
+                            done();
+                            if (typeof result !== 'undefined') {
+                                result.rows.forEach(function(item) {
+                                    // For each row compute hash for each possible pin
+                                    var a = ["primary", "secondary", "company"];
+                                    a.forEach(function(key) {
+                                        if (item.ps[key]) {
+                                            var p = "" + item.ps[key];
+                                            item.ps[key] = crypto.createHash('md5').update(p).digest("hex").substring(0, 8);
+                                        }
+                                    });
+                                });
+                            }
+                            res.header('content-type', 'application/json');
+                            res.send(200, result.rows);
+                        }
+                    }
+                );
+            });
+            //}
+            return next();
+        },
+
+        /**
+         * get list of businesses
+         * @param req
+         * @param res
+         * @param next
+         */
+        getBusinessEmployees: function(req, res, next) {
+            pg.connect(conString, function(err, client, done) {
+
+                if (pgError(err, client)) {
+                    responseError(res, err);
+                    return next(false);
+                }
+
+                var query = "SELECT " +
+                    "employee_id as id, business_code as codice_azienda, business.is_enabled as azienda_abilitata, time_limits as limite_orari " +
+                    "FROM business.business_employee JOIN business.business ON (business_code = code) " +
+                    "WHERE status = 'approved'";
+                client.query(
+                    query, [],
+                    function(err, result) {
+                        if (pgError(err, client)) {
+                            responseError(res, err);
+                        } else {
+                            done();
+                            res.header('content-type', 'application/json');
+                            res.send(200, result.rows);
+                        }
+                    }
+                );
+            });
             return next();
         },
 
